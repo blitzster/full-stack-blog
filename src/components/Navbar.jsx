6d81f6ccb1d9c77f5c6b098ef7ad28f2b56de96d
Akshay <blitz122333@gmail.com>
2025-01-21 19:16:29 +0530

    animation and header done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import { IKImage } from 'imagekitio-react';
 import Image from './Image'
 import { Link } from 'react-router-dom';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 const Navbar = () => {
 
@@ -36,9 +36,15 @@ const Navbar = () => {
             <Link to="/">Trending</Link>
             <Link to="/">Most Popular</Link>
             <Link to="/">About</Link>
-            <Link to="/">
+           
+            <SignedOut>
+            <Link to="/login">
                 <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login</button>
             </Link>
+            </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
         </div>
 
     </div>
