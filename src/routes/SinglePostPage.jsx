1aaa5b1fb7c0c5b74cc68c17f111b5a1fe189447
Akshay <blitz122333@gmail.com>
2025-01-21 18:21:18 +0530

    react routing done

---
+++ src/routes/SinglePostPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SinglePostPage = () => {
+  return (
+    <div className=''>SinglePostPage</div>
+  )
+}
+
+export default SinglePostPage
---
+++ src/routes/Write.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Write = () => {
+  return (
+    <div className=''>Write</div>
+  )
+}
+
+export default Write
